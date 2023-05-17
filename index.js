34245ab785aa044edbd817a5613a65f48ff29a48
topurayhan007 <topurayhan007@gmail.com>
2023-05-17 21:02:14 +0600

    successfully tested API load

--- index.js
+++ index.js
@@ -6,9 +6,46 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+//test
+const demoData = require("./data/test.json");
+
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kon9irj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    client.connect();
+
+    //API CODES HERE
+    app.get("/demo", async (req, res) => {
+      res.send(demoData);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Toy Garden is running");
